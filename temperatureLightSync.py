ec1af2c22784a90770f8b7f24ceef5efa9abf80f
benostendorf1 <150842013+benostendorf1@users.noreply.github.com>
2023-12-06 08:26:52 -0700

    Update temperatureLightSync.py

--- temperatureLightSync.py
+++ temperatureLightSync.py
@@ -17,4 +17,39 @@ result = instance.read()
 
 while True:
   result = instance.read()
-  
+
+  if result.is_valid():
+    print("Temperature: %-3.1f C" % result.temperature)
+    print("Humidity: %-3.1f %%" % result.humidity)
+
+  if result.temperature > 33:
+    red.on()
+    yellow.off()
+    green.off()
+    blue.off()
+    white.off()
+  elif 25 < result.temperature < 33:
+    red.off()
+    yellow.on()
+    green.off()
+    blue.off()
+    white.off()
+  elif 15 < result.temperature < 25:
+    red.off()
+    yellow.off()
+    green.on()
+    blue.off()
+    white.off()
+  elif 0 < result.temperature < 15:
+    red.off()
+    yellow.off()
+    green.off()
+    blue.on()
+    white.off()
+  elif result.temperature < 0:
+    red.off()
+    yellow.off()
+    green.off()
+    blue.off()
+    white.on()
+    
